52631fbd71a96f2ec18e469fa0343a944a9a887c
stzups <griffinht@gmail.com>
2021-12-27 18:11:36 -0500

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,13 @@ async fn main() -> std::io::Result<()> {
             .route("/", actix_web::web::get().to(git))
     });
     eprintln!("initialized");
-    let server = match server.bind(address) {
-        Ok(server) => { server }
-        Err(err) => {return Err(err)}
-    };
+    let server = server.bind(address)?;
     eprintln!("bound to {}", address);
     let future = server.run();
     eprintln!("running");
-    match future.await {
-        Ok(()) => { eprintln!("exited gracefully"); Ok(()) }
-        Err(err) => { Err(err) }
-    }
+    future.await?;
+    eprintln!("exited gracefully");
+    Ok(())
 }
 
 async fn git() -> impl actix_web::Responder {
