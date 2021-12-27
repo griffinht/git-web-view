1fd00a9efd9fff2eb8551914b11bfc261db69c9c
stzups <griffinht@gmail.com>
2021-12-27 18:20:05 -0500

    add verbose variable

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    eprintln!("initializing...");
+    let verbose = true;
+    if verbose { eprintln!("initializing..."); }
     let address = "127.0.0.1:8080";
 
     let server = actix_web::HttpServer::new(|| {
         actix_web::App::new()
             .route("/", actix_web::web::get().to(git))
     });
-    eprintln!("binding to {}...", address);
+    if verbose { eprintln!("binding to {}...", address); }
     let server = server.bind(address)?;
-    eprintln!("running...");
+    if verbose { eprintln!("running..."); }
     let future = server.run();
-    eprintln!("ready to serve");
+    if verbose { eprintln!("ready to serve"); }
     future.await?;
-    eprintln!("exited gracefully");
+    if verbose { eprintln!("exited gracefully"); }
     Ok(())
 }
 
