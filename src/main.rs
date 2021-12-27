ef9942b53de4a9b15d4ecd4febbf98f085da7ae2
stzups <griffinht@gmail.com>
2021-12-27 18:16:44 -0500

    improve verbose log messages?

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    eprintln!("starting git-web-view server");
+    eprintln!("initializing...");
     let address = "127.0.0.1:8080";
 
     let server = actix_web::HttpServer::new(|| {
         actix_web::App::new()
             .route("/", actix_web::web::get().to(git))
     });
-    eprintln!("initialized");
+    eprintln!("binding to {}...", address);
     let server = server.bind(address)?;
-    eprintln!("bound to {}", address);
+    eprintln!("running...");
     let future = server.run();
-    eprintln!("running");
+    eprintln!("ready to serve");
     future.await?;
     eprintln!("exited gracefully");
     Ok(())
