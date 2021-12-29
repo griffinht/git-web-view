155e29f3c87f8d7a0a8ffc9dc47dc04f50f4f5fd
stzups <griffinht@gmail.com>
2021-12-28 20:07:03 -0500

    remove tag from product

--- src/template.rs
+++ src/template.rs
@@ -7,7 +7,8 @@ pub fn parse(path: &str) -> std::io::Result<&str> {
     let mut buf: Vec<u8> = Vec::new();
     loop {
         let read = reader.read_until('$' as u8, &mut buf)?;
-        eprintln!("{}", read);
+        buf.pop();
+        eprintln!("{}", String::from_utf8_lossy(&buf));
         if read == 0 { break; }
 
         let open = &mut [0 as u8; 1];
@@ -20,13 +21,12 @@ pub fn parse(path: &str) -> std::io::Result<&str> {
                 return Err(err);
             }
         };
-        buf.extend_from_slice(open);
         if open[0] != '{' as u8 {
             continue;
         }
-        let start = buf.len();
-        let read = reader.read_until('}' as u8, &mut buf)?;
-        let tag = &buf[start..start + read - 1];
+        let mut tag: Vec<u8> = Vec::new();
+        let read = reader.read_until('}' as u8, &mut tag)?;
+        let tag = &tag[0..read - 1];
         eprintln!("test {}", String::from_utf8_lossy(tag));
     }
     eprintln!("done {}", String::from_utf8_lossy(&buf));
