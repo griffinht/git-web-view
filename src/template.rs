35518fa3fc2f852ceb3378d70f36390c9d8ec012
stzups <griffinht@gmail.com>
2021-12-28 20:11:41 -0500

    improve tag parsing

--- src/template.rs
+++ src/template.rs
@@ -7,8 +7,7 @@ pub fn parse(path: &str) -> std::io::Result<&str> {
     let mut buf: Vec<u8> = Vec::new();
     loop {
         let read = reader.read_until('$' as u8, &mut buf)?;
-        buf.pop();
-        eprintln!("{}", String::from_utf8_lossy(&buf));
+        
         if read == 0 { break; }
 
         let open = &mut [0 as u8; 1];
@@ -22,11 +21,15 @@ pub fn parse(path: &str) -> std::io::Result<&str> {
             }
         };
         if open[0] != '{' as u8 {
+            buf.push(open[0]); // not a tag, so add back the old content
             continue;
         }
+        // now we know it is a tag
+        buf.pop(); // remove $
         let mut tag: Vec<u8> = Vec::new();
         let read = reader.read_until('}' as u8, &mut tag)?;
         let tag = &tag[0..read - 1];
+        buf.extend_from_slice("TEST TAG CONTENT".as_bytes());
         eprintln!("test {}", String::from_utf8_lossy(tag));
     }
     eprintln!("done {}", String::from_utf8_lossy(&buf));
