a8d040c9198a0aee2672b3288b52d230c4d6f1b2
stzups <griffinht@gmail.com>
2021-12-31 18:43:40 -0500

    move git.rs

---
+++ src/git.rs
@@ -0,0 +1,58 @@
+pub async fn git(request: actix_web::HttpRequest, state: actix_web::web::Data<crate::State>) -> impl actix_web::Responder {
+    eprintln!("{} {} {}", request.peer_addr().unwrap(), request.method(), request.path());
+    //todo prevent filesystem traversal with ../../.. or something
+    let path = format!("./{}", request.path());
+    let metadata = match std::fs::metadata(&path) {
+        Ok(file) => { file }
+        Err(err) => { eprintln!("{}", err); return actix_web::HttpResponse::NotFound().finish(); }
+    };
+    //todo symlink support?
+
+    let template_name;
+    if metadata.is_dir() { template_name = "directory.html"; }
+    else if metadata.is_file() { template_name = "file.html"; }
+    else { eprintln!("not a file or a directory"); return actix_web::HttpResponse::NotFound().finish(); }
+    let template = state.template.get(template_name);
+
+    if template.is_none() { eprintln!("no template for {}", template_name); return actix_web::HttpResponse::InternalServerError().finish(); }
+    let template = template.unwrap();
+    let mut body: Vec<u8> = Vec::new();
+    for parsed in template {
+        body.extend_from_slice(&*parsed.buf);
+        match &parsed.tag {
+            None => { }
+            Some(tag) => {
+                match tag.as_str() {
+                    "NAV" => {
+                        body.extend_from_slice(&crate::template::nav::get_nav(request.path()));
+                    }
+                    "DIRECTORY" => { body.extend_from_slice(&crate::template::links::get_links(&path).unwrap()); }
+                    "PATH" => { body.extend_from_slice(path.as_bytes()); }
+                    "FILE" => {
+                        let escape_html = true;
+                        if escape_html {
+                            body.extend(match std::fs::read_to_string(&path) {
+                                Ok(f) => { f }
+                                Err(err) => { eprintln!("error reading file to string: {}", err); return actix_web::HttpResponse::NotFound().finish(); }
+                            }
+                                .replace("&", "&amp")//todo each replace is very slow
+                                .replace("<", "&lt")
+                                .replace(">", "&gt")
+                                .as_bytes());
+                        } else {
+                            body.extend(&match std::fs::read(&path) {
+                                Ok(f) => { f }
+                                Err(err) => {
+                                    eprintln!("error reading file: {}", err);
+                                    return actix_web::HttpResponse::NotFound().finish();
+                                }
+                            })
+                        }
+                    }
+                    _ => { eprintln!("unknown tag {}", tag); return actix_web::HttpResponse::InternalServerError().finish(); }
+                }
+            }
+        };
+    }
+    return actix_web::HttpResponse::Ok().content_type("text/html").body(body);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 mod options;
 mod template;
+mod git;
 
 #[macro_export]
 macro_rules! default_bind_address {
     () => ("127.0.0.1:8080".to_string())
 }
 
-struct State {
+pub struct State {
     template: std::collections::HashMap<String, Vec<template::Parsed>>
 }
 
@@ -35,7 +36,7 @@ async fn main() -> std::io::Result<()> {
                 template::parse_directory_default()
             }
             })
-            .route("/*", actix_web::web::get().to(git))
+            .route("/*", actix_web::web::get().to(git::git))
             .wrap(actix_web::middleware::Compress::new(
                 if matches.opt_present("disable-compression") {
                     actix_web::http::ContentEncoding::Identity
@@ -51,63 +52,4 @@ async fn main() -> std::io::Result<()> {
     future.await?;
     if verbose { eprintln!("exited gracefully"); }
     Ok(())
-}
-
-async fn git(request: actix_web::HttpRequest, state: actix_web::web::Data<State>) -> impl actix_web::Responder {
-    eprintln!("{} {} {}", request.peer_addr().unwrap(), request.method(), request.path());
-    //todo prevent filesystem traversal with ../../.. or something
-    let path = format!("./{}", request.path());
-    let metadata = match std::fs::metadata(&path) {
-        Ok(file) => { file }
-        Err(err) => { eprintln!("{}", err); return actix_web::HttpResponse::NotFound().finish(); }
-    };
-    //todo symlink support?
-
-    let template_name;
-    if metadata.is_dir() { template_name = "directory.html"; }
-    else if metadata.is_file() { template_name = "file.html"; }
-    else { eprintln!("not a file or a directory"); return actix_web::HttpResponse::NotFound().finish(); }
-    let template = state.template.get(template_name);
-
-    if template.is_none() { eprintln!("no template for {}", template_name); return actix_web::HttpResponse::InternalServerError().finish(); }
-    let template = template.unwrap();
-    let mut body: Vec<u8> = Vec::new();
-    for parsed in template {
-        body.extend_from_slice(&*parsed.buf);
-        match &parsed.tag {
-            None => { }
-            Some(tag) => {
-                match tag.as_str() {
-                    "NAV" => {
-                        body.extend_from_slice(&template::nav::get_nav(request.path()));
-                    }
-                    "DIRECTORY" => { body.extend_from_slice(&template::links::get_links(&path).unwrap()); }
-                    "PATH" => { body.extend_from_slice(path.as_bytes()); }
-                    "FILE" => {
-                        let escape_html = true;
-                        if escape_html {
-                            body.extend(match std::fs::read_to_string(&path) {
-                                Ok(f) => { f }
-                                Err(err) => { eprintln!("error reading file to string: {}", err); return actix_web::HttpResponse::NotFound().finish(); }
-                            }
-                                .replace("&", "&amp")//todo each replace is very slow
-                                .replace("<", "&lt")
-                                .replace(">", "&gt")
-                                .as_bytes());
-                        } else {
-                            body.extend(&match std::fs::read(&path) {
-                                Ok(f) => { f }
-                                Err(err) => {
-                                    eprintln!("error reading file: {}", err);
-                                    return actix_web::HttpResponse::NotFound().finish();
-                                }
-                            })
-                        }
-                    }
-                    _ => { eprintln!("unknown tag {}", tag); return actix_web::HttpResponse::InternalServerError().finish(); }
-                }
-            }
-        };
-    }
-    return actix_web::HttpResponse::Ok().content_type("text/html").body(body);
 }
