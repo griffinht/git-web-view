90741fccd74c0d49b523658aef1f39824045740a
stzups <griffinht@gmail.com>
2022-01-02 15:49:44 -0500

    fix file relative path

--- src/template/nav.rs
+++ src/template/nav.rs
@@ -8,21 +8,27 @@ pub fn get_nav(request_path: &str) -> Vec<u8> {
         }
         return dots.join("");
     }
-    fn get_directory_link(directory: &str, i: i32, trailing: &str) -> String {
+    fn get_directory_link(directory: &str, is_file: bool, i: i32, trailing: &str) -> String {
         if i == 0 { // trailing (current) file/directory should not be a link
             format!("{}{}", directory, trailing)
         } else {
-            format!("<a href=\"{}\">{}{}</a>", get_repeated_string("../", i), directory, trailing)
+            let string = if i == 1 && is_file {
+                "./"
+            } else {
+                "../"
+            };
+            format!("<a href=\"{}\">{}{}</a>", get_repeated_string(string, i), directory, trailing)
         }
     }
+    let is_bool = !request_path.ends_with("/");
     for directory in request_path.rsplit("/") {
         if directory.is_empty() { continue; }
         // /example/dir/ <- needs to be added back for directories
-        nav.push(get_directory_link(directory, i, "/"));
+        nav.push(get_directory_link(directory, is_bool, i, "/"));
         i = i + 1;
     }
     // add leading slash -> /example/dir/
-    nav.push(get_directory_link("/", i, ""));
+    nav.push(get_directory_link("/", is_bool, i, ""));
     nav.reverse();
     let nav = nav;
     let mut real_nav: Vec<u8> = Vec::new();
